c8eab0454fcfd2baa871ab2683dec8da9d58f2b4
Tlotlo Golelelwang <tlotlogolelelwang@gmail.com>
2025-02-05 10:39:07 +0200

    Skills and Portfolio
    Updating my skills and portfolios projects

--- src/Pages/Home/MyPortfolio.js
+++ src/Pages/Home/MyPortfolio.js
@@ -1,47 +1,45 @@
-import data from "../../data/index.json"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import data from "../../data/index.json";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function MyPortfolio(){
     return(
-        <section id ="MyPortfolio" className="portfolio-section">
+        <section id="MyPortfolio" className="portfolio-section">
             <div className="portfolio-container-box">
                 <div className="portfolio-container">
-                   <p className="sub-title"> Recent Projects
-                   </p> 
+                   <p className="sub-title"> Recent Projects</p> 
                 </div>
                 <h2 className="section-heading">
                     My Portfolio
                 </h2>
             </div> 
             <div>
-               <button className="btn btn-github">
-               <FontAwesomeIcon icon={faGithub} size ="xl"/> Visit My GitHub
-               </button> 
+               <a href="https://github.com/TGolelelwang" target="_blank" rel="noopener noreferrer">
+                   <button className="btn btn-github">
+                       <FontAwesomeIcon icon={faGithub} size="xl" /> Visit My GitHub
+                   </button> 
+               </a>
             </div>
             <div className="portfolio-section-container">
-                {data?.portfolio?.map((item, index)=>{
-                   return (
-                    <div key={index} className="portfolio-section-card">
-                        <div className="portfolio-section-img">
-                            <img src={item.src} alt="Placeholder" /> {/* Corrected 'srs' to 'src' */}
-                        </div>
-                        <div className="portfolio-section-card-content">
-                            <div>
-                                <h3 className="portfolio-section-title">{item.title}</h3>
-                                <p className="text-md">{item.description}</p>
+                {data?.portfolio?.map((item, index) => {
+                    return (
+                        <div key={index} className="portfolio-section-card">
+                            <div className="portfolio-section-img">
+                                <img src={item.src} alt="Placeholder" /> {/* Corrected 'srs' to 'src' */}
                             </div>
-                            <p className="text-sm portfolio-link"></p>
+                            <div className="portfolio-section-card-content">
+                                <div>
+                                    <h3 className="portfolio-section-title">{item.title}</h3>
+                                    <p className="text-md">{item.description}</p>
+                                </div>
+                                <p className="text-sm portfolio-link"></p>
+                            </div>
                         </div>
-                    </div>
-                );
-                }
-                
-                
-                )}
+                    );
+                })}
             </div> 
         </section>
-    )
+    );
 }
 
-export default MyPortfolio
+export default MyPortfolio;
