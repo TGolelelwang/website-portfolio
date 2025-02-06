7ebf5d26d98ac538059c589e653f54eb501f02c6
Tlotlo Golelelwang <tlotlogolelelwang@gmail.com>
2025-02-06 09:40:39 +0200

    Update AboutMe

--- src/Pages/Home/AboutMe.js
+++ src/Pages/Home/AboutMe.js
@@ -14,10 +14,11 @@ function AboutMe(){
                     </h1>
                 </div>
                 <p className="hero-section-description">
-                    lorem30 duhadfadfad agkadlga dgdadaodf adfdf
-                    adfaodh adufhaodf adf aodfhaduof adf.
-                    adfljadnfajdfhuoadf
-                    fadfadlfjhadofa
+                    Why Data Analytics, well simply because I tried Development and I did enjoy it.
+                    But I found that I enjoyed the data more than the development.
+                    <br></br>
+
+                    And that is how we I got here. The concept of finding data, querying it (finding what it has to say) and then having to present the findings and communicating those insights really excites me.
 
                 </p>
                 <p className="hero-section-description">
